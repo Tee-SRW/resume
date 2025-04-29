8867c4c37e87bd0526823433e1d37ee37751c057
TeeRex <61543306+Tee-SRW@users.noreply.github.com>
2025-04-29 14:29:10 +0700

    add picture and styling website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,26 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'project-angular';
+  selectedTab: string = 'about';
+
+  tabChanged(tab: string): void {
+    switch (tab) {
+      case 'about':
+        this.selectedTab = 'about';
+        break;
+      case 'exprience':
+        this.selectedTab = 'exprience';
+        break;
+      case 'skills':
+        this.selectedTab = 'skills';
+        break;
+      case 'education':
+        this.selectedTab = 'education';
+        break;
+    }
+  }
 }
